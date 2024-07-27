bcbe9848967d6547b2511244ec02196b9fddecac
thelordzeus <arnav55125@gmail.com>
2024-07-28 02:10:14 +0530

    feat: MAJOR VERSIONING FIX

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:chat_application/auth/login_or_register.dart';
 import 'package:chat_application/firebase_options.dart';
 import 'package:chat_application/themes/light_mode.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load the .env file
+  try {
+    await dotenv.load(fileName: ".env");
+    print('Loaded .env file successfully');
+  } catch (e) {
+    print('Error loading .env file: $e');
+  }
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
