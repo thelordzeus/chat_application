93854204e93cb688158db9a0fa1cc2a2dc5e8feb
thelordzeus <arnav55125@gmail.com>
2024-07-27 18:44:28 +0530

    major fix

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -40,47 +41,49 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'AIzaSyBakeyLCt80IFG6QF3EC81dfiKZO_AE2Yk',
-    appId: '1:680209913954:web:3ddd10c849e5dbf2dc593b',
-    messagingSenderId: '680209913954',
-    projectId: 'chatapp-dacf1',
-    authDomain: 'chatapp-dacf1.firebaseapp.com',
-    storageBucket: 'chatapp-dacf1.appspot.com',
-  );
+  static FirebaseOptions get web => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_WEB_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_WEB_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_WEB_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_WEB_PROJECT_ID'] ?? '',
+        authDomain: dotenv.env['FIREBASE_WEB_AUTH_DOMAIN'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_WEB_STORAGE_BUCKET'] ?? '',
+      );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAXXZ_DMOAdpwHvqvubZjCeWkGCrPbkDXg',
-    appId: '1:680209913954:android:dbaf4a12f6b16913dc593b',
-    messagingSenderId: '680209913954',
-    projectId: 'chatapp-dacf1',
-    storageBucket: 'chatapp-dacf1.appspot.com',
-  );
+  static FirebaseOptions get android => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_ANDROID_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_ANDROID_APP_ID'] ?? '',
+        messagingSenderId:
+            dotenv.env['FIREBASE_ANDROID_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_ANDROID_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_ANDROID_STORAGE_BUCKET'] ?? '',
+      );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBok4gV-h6LgkE5JuLZCwu0YmnlXFEKYdA',
-    appId: '1:680209913954:ios:cd1f9d818ad65335dc593b',
-    messagingSenderId: '680209913954',
-    projectId: 'chatapp-dacf1',
-    storageBucket: 'chatapp-dacf1.appspot.com',
-    iosBundleId: 'com.example.chatApplication',
-  );
+  static FirebaseOptions get ios => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_IOS_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_IOS_APP_ID'] ?? '',
+        messagingSenderId: dotenv.env['FIREBASE_IOS_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_IOS_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_IOS_STORAGE_BUCKET'] ?? '',
+        iosBundleId: dotenv.env['FIREBASE_IOS_BUNDLE_ID'] ?? '',
+      );
+  static FirebaseOptions get macos => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_MACOS_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_MACOS_APP_ID'] ?? '',
+        messagingSenderId:
+            dotenv.env['FIREBASE_MACOS_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_MACOS_PROJECT_ID'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_MACOS_STORAGE_BUCKET'] ?? '',
+        iosBundleId: dotenv.env['FIREBASE_MACOS_BUNDLE_ID'] ?? '',
+      );
 
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyBok4gV-h6LgkE5JuLZCwu0YmnlXFEKYdA',
-    appId: '1:680209913954:ios:cd1f9d818ad65335dc593b',
-    messagingSenderId: '680209913954',
-    projectId: 'chatapp-dacf1',
-    storageBucket: 'chatapp-dacf1.appspot.com',
-    iosBundleId: 'com.example.chatApplication',
-  );
-
-  static const FirebaseOptions windows = FirebaseOptions(
-    apiKey: 'AIzaSyBakeyLCt80IFG6QF3EC81dfiKZO_AE2Yk',
-    appId: '1:680209913954:web:20f4651ef528d610dc593b',
-    messagingSenderId: '680209913954',
-    projectId: 'chatapp-dacf1',
-    authDomain: 'chatapp-dacf1.firebaseapp.com',
-    storageBucket: 'chatapp-dacf1.appspot.com',
-  );
+  static FirebaseOptions get windows => FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_WINDOWS_API_KEY'] ?? '',
+        appId: dotenv.env['FIREBASE_WINDOWS_APP_ID'] ?? '',
+        messagingSenderId:
+            dotenv.env['FIREBASE_WINDOWS_MESSAGING_SENDER_ID'] ?? '',
+        projectId: dotenv.env['FIREBASE_WINDOWS_PROJECT_ID'] ?? '',
+        authDomain: dotenv.env['FIREBASE_WINDOWS_AUTH_DOMAIN'] ?? '',
+        storageBucket: dotenv.env['FIREBASE_WINDOWS_STORAGE_BUCKET'] ?? '',
+      );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:chat_application/firebase_options.dart';
 import 'package:chat_application/themes/light_mode.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
