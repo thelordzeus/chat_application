cc361be478fa22403da0a3fbb81991f9b3b3febf
thelordzeus <arnav55125@gmail.com>
2024-07-27 17:47:33 +0530

    feat: added textfield and textControllers

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,67 @@
+import 'package:chat_application/components/text_field_custom.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  // email and password controller
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+
+            const SizedBox(
+              height: 50,
+            ),
+
+            //welcome back message
+            Text(
+              "Welcome back, you've been missed!",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.primary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(
+              height: 50,
+            ),
+
+            // pw textfield
+            CTextField(
+              hintText: "Email",
+              obscureText: false,
+              controller: _emailController,
+            ),
+
+            const SizedBox(
+              height: 10,
+            ),
+
+            CTextField(
+              hintText: "Password",
+              obscureText: true,
+              controller: _passwordController,
+            ),
+
+            //login button
+
+            //register now
+          ],
+        ),
+      ),
     );
   }
 }
