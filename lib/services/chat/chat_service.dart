3044fd3fe37883553c65c5f3505126599d5ca418
thelordzeus <arnav55125@gmail.com>
2024-07-31 23:46:48 +0530

    feat: added chat service files

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,5 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ChatService {
   // get instance of firestore
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  // get user stream
+  // // get user stream
+  // Stream<List<Map<String,dynamic>>> getUsersSteam(){
+
+  // }
 }
